16c280527c16897af0cd5a6d872ba143588c058e
Theigrams <theigrams@buaa.edu.cn>
2024-06-23 03:06:43 +0800

    rename feedrate_planner to look_ahead

--- core/look_ahead.py
+++ core/look_ahead.py
@@ -23,6 +23,8 @@ class BidirectionalScanner:
         self.a_max = a_max
         self.j_max = j_max
         self.v_limit = self.path.get_v_limit(dt, self.v_max, self.a_max, j_max=j_max)
+        self.backward_scan()
+        self.forward_scan()
 
     def backward_scan(self):
         self.v_limit[-1] = 0
