1ddb9c9620f7af155b8043bade21210db2a18077
Theigrams <sy1909148@buaa.edu.cn>
2024-09-19 19:31:10 +0800

    feat: enhance data structure for interpolation, enabling more detailed output including velocity, acceleration, and direction

--- core/interpolator.py
+++ core/interpolator.py
@@ -13,30 +13,44 @@ class Interpolator:
         self.N = len(path.blocks)
         self.blocks = path.blocks
         self.profiles = self.scheduler.profiles
+        self.dim = path.points.shape[1]
+        self.dtype = [
+            ("T", np.float64),
+            ("P", np.float64, (self.dim,)),
+            ("V", np.float64),
+            ("A", np.float64),
+            ("J", np.float64),
+            ("D", np.float64, (self.dim,)),
+        ]
+        self.data = None
 
     def interpolate(self):
-        interpolated_points = []
+        interpolated_data = np.empty(0, dtype=self.dtype)
         remaining_time = 0
         for i in range(self.N):
             block = self.blocks[i]
             profile = self.profiles[i]
-            block_points, remaining_time = self.interpolate_block(block, profile, remaining_time)
-            interpolated_points += block_points
-        return np.array(interpolated_points)
+            block_data, remaining_time = self.interpolate_block(block, profile, remaining_time)
+            interpolated_data = np.concatenate((interpolated_data, block_data))
+        self.data = interpolated_data
+        return self.data["P"]
 
     def interpolate_block(self, block: Block, profile: FeedrateProfile, remaining_time: float):
         total_time = profile.total_time
-        interpolated_points = []
+        interpolated_data = np.empty((0,), dtype=self.dtype)
         t = remaining_time
         while t < total_time:
             s, v, a, j = profile.get_motion_state(t)
             u, idx = block.get_u_from_length(s)
             point = block.curves[idx](u)
-            interpolated_points.append(point)
+            tangent = block.curves[idx].derivative(u)
+            direction = tangent / (np.linalg.norm(tangent) + 1e-10)
+            data = np.array([(t, point, v, a, j, direction)], dtype=self.dtype)
+            interpolated_data = np.append(interpolated_data, data)
             t += self.Ts
         remaining_time = t - total_time
 
-        return interpolated_points, remaining_time
+        return interpolated_data, remaining_time
 
 
 if __name__ == "__main__":
